b25cd1340cb8d1779dbe396bc52b8a1eaa58ab23
webusinka <veronikabykova05@gmail.com>
2024-10-04 23:57:37 +0700

    Added interface file instead of tests

--- Array.hpp
+++ Array.hpp
@@ -3,142 +3,144 @@
 
 template <typename Data>
 class Array {
-    public:
+public:
+    Data* array;
+    int size;       // размерность массива
+    int capacity; // текущая ёмкость массива
 
-        Data* array;
-        int size;       // размерность массива
-        int capacity; // текущая ёмкость массива
+    Array() : array(nullptr), size(0), capacity(10) {}
 
-        Array() : array(nullptr), size(0), capacity(0) {}
+    ~Array() {
+        delete[] array;
+    }
 
-        ~Array() {
+    void push_front(const Data& value){ //добавление в начало массива
+        if (size == capacity) {
+            int newCapacity = capacity * 2;
+            Data* newArray = new Data[newCapacity];
+
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+
+            delete[] array; 
+            array = newArray;
+            capacity = newCapacity;
+        }
+        for (int i = size; i > 0; i--) { 
+            array[i] = array[i - 1];
+        }
+
+        array[0] = value;
+        size++;
+    }
+
+    void push_back(const Data& value){
+        if (size == capacity) {
+            int newCapacity = capacity * 2;
+            Data* newArray = new Data[newCapacity];
+
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+
             delete[] array;
+            array = newArray;
+            capacity = newCapacity;
         }
 
-        void initialize(Array& checklist, int initialCapacity){
-            checklist.capacity = initialCapacity; 
-            checklist.size = 0;
-            checklist.array = new Data[checklist.capacity]; 
+        array[size] = value;
+        size++;
+    }
+
+    void push_in(int index, const Data& value){
+        if (index < 0 || index > size) {
+            std::cout << "Invalid index" << std::endl;
+            return;
         }
-        void push_front(Array& checklist, const Data& value){ //добавление в начало массива
-                if (checklist.size == checklist.capacity) {
-                    int newCapacity = checklist.capacity * 2;
-                    Data* newArray = new Data[newCapacity];
 
-                    for (int i = 0; i < checklist.size; i++) {
-                        newArray[i] = checklist.array[i];
-                    }
+        if (size == capacity) {
+            int newCapacity = capacity * 2;
+            Data* newArray = new Data[newCapacity];
 
-                    delete[] checklist.array; 
-                    checklist.array = newArray;
-                    checklist.capacity = newCapacity;
-                }
-                for (int i = checklist.size; i > 0; i--) { 
-                    checklist.array[i] = checklist.array[i - 1];
-                }
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
 
-                checklist.array[0] = value;
-                checklist.size++;
+            delete[] array;
+            array = newArray;
+            capacity = newCapacity;
         }
-        void push_back(Array& checklist, const Data& value){
-                if (checklist.size == checklist.capacity) {
-                    int newCapacity = checklist.capacity * 2;
-                    Data* newArray = new Data[newCapacity];
 
-                    for (int i = 0; i < checklist.size; i++) {
-                        newArray[i] = checklist.array[i];
-                    }
-
-                    delete[] checklist.array;
-                    checklist.array = newArray;
-                    checklist.capacity = newCapacity;
-                }
-
-                checklist.array[checklist.size] = value;
-                checklist.size++;
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
         }
-        void push_in(Array& checklist, int index, const Data& value){
-            if (index < 0 || index > checklist.size) {
-                std::cout << "Invalid index" << std::endl;
+
+        array[index] = value;
+        size++;
+    }
+
+    void pop_front(){
+        if (size <= 0) {
+            std::cout << "Array is empty, cannot pop front." << std::endl;
+        } else {
+            for (int i = 0; i < size - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
+    void pop_back(){
+        if (size > 0) {
+            size--;
+        } else {
+            std::cout << "Array is empty, cannot pop back." << std::endl;
+        }
+    }
+
+    void pop_in(int index){
+        if (index < 0 || index >= size) {
+            std::cout << "Invalid index" << std::endl;
+            return;
+        }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+    }
+
+    void search(const Data& value){
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                std::cout << "Found!" << std::endl;
                 return;
             }
+        }
+        std::cout << "Not found!" << std::endl;
+    }
 
-            if (checklist.size == checklist.capacity) {
-                int newCapacity = checklist.capacity * 2;
-                Data* newArray = new Data[newCapacity];
+    void search_index(const int index){
+        if (index >= 0 && index < size) {
+            std::cout << array[index] << std::endl;
+        } else {
+            std::cout << "Invalid index" << std::endl;
+        }
+    }
 
-                for (int i = 0; i < checklist.size; i++) {
-                    newArray[i] = checklist.array[i];
-                }
+    void display(){
+        for (int i = 0; i < size; i++) {
+            std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 
-                delete[] checklist.array;
-                checklist.array = newArray;
-                checklist.capacity = newCapacity;
-            }
-
-            for (int i = checklist.size; i > index; i--) {
-                checklist.array[i] = checklist.array[i - 1];
-            }
-
-            checklist.array[index] = value;
-            checklist.size++;
+    void set(const int index, Data value){
+        if (index >= 0 && index <= size) {
+            array[index] = value;
+            std::cout << array[index] << std::endl;
+        } else {
+            std::cout << "Invalid index" << std::endl; 
         }
-        void pop_front(Array& checklist){
-            if (checklist.size <= 0) {
-                std::cout << "Array is empty, cannot pop front." << std::endl;
-            } else {
-                for (int i = 0; i < checklist.size - 1; i++) {
-                    checklist.array[i] = checklist.array[i + 1];
-                }
-                checklist.size--;
-            }
-        }
-        void pop_back(Array& checklist){
-            if (checklist.size > 0) {
-                checklist.size--;
-            } else {
-                std::cout << "Array is empty, cannot pop back." << std::endl;
-            }
-        }
-        void pop_in(Array& checklist, int index){
-            if (index < 0 || index >= checklist.size) {
-                std::cout << "Invalid index" << std::endl;
-                return;
-            }
-            for (int i = index; i < checklist.size - 1; i++) {
-                checklist.array[i] = checklist.array[i + 1];
-            }
-            checklist.size--;
-        }
-        void search(const Array& checklist, const Data& value){
-            for (int i = 0; i < checklist.size; i++) {
-                if (checklist.array[i] == value) {
-                    std::cout << "Found!" << std::endl;
-                    return;
-                }
-            }
-            std::cout << "Not found!" << std::endl;
-        }
-        void search_index(const Array& checklist, const int index){
-            if (index >= 0 && index < checklist.size) {
-                std::cout << checklist.array[index] << std::endl;
-            } else {
-                std::cout << "Invalid index" << std::endl;
-            }
-        }
-        void display(const Array& checklist){
-            for (int i = 0; i < checklist.size; i++) {
-                std::cout << checklist.array[i] << " ";
-            }
-            std::cout << std::endl;
-
-        }
-        void set(const Array& checklist, const int index, Data value){
-            if (index >= 0 && index <= size) {
-                checklist.array[index] = value;
-                std::cout << checklist.array[index] << std::endl;
-            } else {
-                std::cout << "Invalid index" << std::endl; 
-            }
-        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <fstream>
+#include <sys/wait.h>
 
 #include "Array.hpp"
 #include "List.hpp"
@@ -12,177 +15,434 @@
 
 using namespace std;
 
-int main(){
+map<string, Array<string>> arrays;
+map<string, LinkedList<string>> linked_lists;
+map<string, DoublyList<string>> doubly_lists;
+map<string, Stack<string>> stacks;
+map<string, Queue<string>> queues;
+map<string, Hash_map<string, string, 1000>> hash_tables;
+map<string, AVLTree<int>> trees;
 
-    std::cout << "--------------ARRAY-------------" << std::endl;
-    Array<int> arr;
-    arr.initialize(arr, 5); //емкость 5
+// Обработка команды
+void processQuery(const string& query) {
+    stringstream ss(query);
+    string command;
+    ss >> command;
+    // ----------ARRAY------
 
-    arr.push_back(arr, 1); // Добавление в конец
-    arr.push_back(arr, 2);
-    arr.push_back(arr, 3);
-    arr.display(arr);
-
-    arr.push_front(arr, 8);
-    arr.push_front(arr, 9);//добавление в начало
-    arr.display(arr);
-
-    arr.search(arr, 1);
-    arr.search(arr, 17);
-
-    arr.push_in(arr, 1, 15); // Вставка по индексу
-    arr.display(arr); // Вывод
-
-    arr.pop_in(arr, 2); // Удаляем элемент 2
-    arr.display(arr);
-
-    arr.pop_back(arr);//удаляем последний
-    arr.display(arr);
-
-    arr.pop_front(arr);//удаляем первый
-    arr.display(arr);
-
-    arr.search(arr, 15); // Поиск
-    arr.search(arr, 50);
-
-    arr.set(arr, 2, 100);
-    arr.display(arr);
-
-    std::cout << "-------------LINKED LIST-------------" << std::endl;
-    LinkedList<string> list;
-    list.push_back("1");
-    list.push_front("2");
-    list.push_back("3");
-    list.push_back("4");
-    list.insert(3, "5");
-    list.display();//заполнение списка значениями
-
-    list.push_front("7");//вставка в начало
-
-    list.search("3");//поиск по значению
-    list.pop_value("4");
-
-    list.display();
-    list.pop_head();//удавление первого
-    list.display();
-    list.pop_tail();//удаление последнего
-    list.display();
-    list.pop(1);//удаление по индексу
-    list.display();
-
-    std::cout << "-------------DOUBLY LINKED LIST-------------" << std::endl;
-    DoublyList<int> doublyList;
-    doublyList.push_back(1);
-    doublyList.push_back(2);
-    doublyList.push_back(3);
-    doublyList.push_in(1, 8);
-    doublyList.push_back(4);
-    doublyList.push_front(9);//заполнение двусвязного списка
-    doublyList.display();
-
-    doublyList.search(4);//поиск по значению
-    doublyList.search(15);
-
-    doublyList.pop_forward();//удалить первый
-    doublyList.display();
-
-    doublyList.pop_backward();//удалить последний
-    doublyList.display();
-
-
-    doublyList.pop(2);
-    doublyList.pop_value(3);//удалить
-    doublyList.display();
-
-    std::cout << "-------------QUEUE-------------" << std::endl;
-    Queue<string> dq;
-    dq.push("5");
-    dq.push("4");
-    dq.push("3");
-    dq.push("2");
-    dq.push("1");//заполнение очереди
-    dq.display();
-
-    dq.search("5");//поиск по значению
-    dq.search("8");
-
-    dq.pop();//удаление
-    dq.pop();
-    dq.display();
-
-    std::cout << "-------------STACK-------------" << std::endl;
-    Stack<int> st;
-    st.push(5);
-    st.push(4);
-    st.push(3);
-    st.push(2);
-    st.push(1);//заполнение стека
-    st.display();
-
-    st.search(1);//поиск в стеке по значению
-    st.search(0);
-
-    st.pop();//последним пришел - первым выйдешь
-    st.display();
-
-    std::cout << "-------------HASH TABLE-------------" << std::endl;
-    Hash_map<int, std::string, 10> table;
-    table.insert(1, "one");
-    table.insert(2, "two");
-    table.insert(3, "three");//заполнение
-
-    std::string value;
-    if (table.get_value(3, value)) {
-        std::cout << "Value for key 3: " << value << std::endl;
-    } else {
-        std::cout << "Key 3 not found" << std::endl;
+    // push_back(string value)
+    // push_front(string value)
+    // push_in(int index, string value)
+    // pop_front()
+    // pop_back()
+    // pop_in(int index)
+    // search(string value)
+    // search_index(int index)
+    // void display();
+    // set(int index, string value)
+    if (command == "APUSH"){ // добавление элемента в конец
+        string name, value;
+        ss >> name >> value;
+        arrays[name].push_back(value);
+    }
+    else if (command == "APUSH_FRONT"){ // добавление элемента в начало
+        string name, value;
+        ss >> name >> value;
+        arrays[name].push_front(value);
+    }
+    else if (command == "AINSERT"){ // добавление элемента по индексу
+        string name, value;
+        int index;
+        ss >> name >> index >> value;
+        arrays[name].push_in(index, value);
+    }
+    else if (command == "APOP"){ // удаление элемента по индексу
+        string name;
+        int index;
+        ss >> name >> index;
+        arrays[name].pop_in(index);
+    }
+    else if (command == "APOP_FRONT"){ // удаление элемента из начала
+        string name;
+        ss >> name;
+        arrays[name].pop_front();
+    }
+    else if (command == "APOP_BACK"){ // удаление по индексу
+        string name;
+        ss >> name;
+        arrays[name].pop_back();
+    }
+    else if (command == "AREPLACE"){ // замена элемента по индексу - set
+        string name, value;
+        int index;
+        ss >> name >> index >> value;
+        arrays[name].set(index, value);
+    }
+    else if (command == "AGET"){ // получение элемента по индексу - search_index
+        string name;
+        int index;
+        ss >> name >> index;
+        arrays[name].search_index(index);
+    }
+    else if (command == "ASEARCH"){ // есть ли элемент по индексу 
+        string name, value;
+        ss >> name  >> value;
+        arrays[name].search(value);
+    }
+    else if (command == "APRINT"){ // вывод на экран
+        string name;
+        ss >> name;
+        arrays[name].display();
     }
 
-    table.remove(2);
+    // ----------LINKEDLIST-------
 
-    if (table.get_value(2, value)) {
-        std::cout << "Value for key 2: " << value << std::endl;
-    } else {
-        std::cout << "Key 2 not found" << std::endl;
+    // push_front(string value) // добавление элемента в голову
+    // push_back(string value) // добавление элемента в хвост
+    // insert(int index, string value) // добавление элемента по индексу
+    // pop_head() // удаление элемента с головы
+    // pop_tail() // удаление элемента с хвоста
+    // pop(int index) // удаление элемента по индексу
+    // pop_value(Data value) // удаление элемента по значению
+    // search(Data value) // поиск элемента по значению
+    // void display();
+    else if (command == "LPUSH_HEAD"){ // добавление элемента в голову
+        string name, value;
+        ss >> name >> value;
+        linked_lists[name].push_front(value);
+    }
+    else if (command == "LPUSH_TAIL"){ // добавление элемента в хвост
+        string name, value;
+        ss >> name >> value;
+        linked_lists[name].push_back(value);
+    }
+    else if (command == "LINSERT"){ // добавление элемента по индексу
+        string name, value;
+        int index;
+        ss >> name >> index >> value;
+        linked_lists[name].insert(index, value);
+    }
+    else if (command == "LPOP_HEAD"){ // удаление элемента с головы
+        string name;
+        ss >> name;
+        linked_lists[name].pop_head();
+    }
+    else if (command == "LPOP_TAIL"){ // удаление элемента с хвоста
+        string name;
+        ss >> name;
+        linked_lists[name].pop_tail();
+    }
+    else if (command == "LPOP"){ // удаление элемента с хвоста
+        string name;
+        int index;
+        ss >> name >> index;
+        linked_lists[name].pop(index);
+    }
+    else if (command == "LPOP_VALUE"){ // удаление элемента по значению
+        string name, value;
+        ss >> name >> value;
+        linked_lists[name].pop_value(value);
+    }
+    else if (command == "LSEARCH"){ // поиск элемента по значению
+        string name, value;
+        ss >> name >> value;
+        linked_lists[name].search(value);
+    }
+    else if (command == "LPRINT"){ // вывод на экран
+        string name;
+        ss >> name;
+        linked_lists[name].display();
     }
 
-    table.insert(1, "new one");
+        // ----------DOUBLYLIST-------
 
-    if (table.get_value(1, value)) {
-        std::cout << "Value for key 1: " << value << std::endl;
-    } else {
-        std::cout << "Key 1 not found" << std::endl;
+    // push_front(string value) // добавление элемента в голову
+    // push_back(string value) // добавление элемента в хвост
+    // push_in(int index, string value) // добавление элемента по индексу
+    // search(Data value) // поиск элемента по значению
+    // pop_forward() // удаление элемента с головы
+    // pop_backward() // удаление элемента с хвоста
+    // pop(int index) // удаление элемента по индексу
+    // pop_value(Data value) // удаление элемента по значению
+    // void display();
+    else if (command == "2LPUSH_HEAD"){ // добавление элемента в голову
+        string name, value;
+        ss >> name >> value;
+        doubly_lists[name].push_front(value);
+    }
+    else if (command == "2LPUSH_TAIL"){ // добавление элемента в хвост
+        string name, value;
+        ss >> name >> value;
+        doubly_lists[name].push_back(value);
+    }
+    else if (command == "2LINSERT"){ // добавление элемента в хвост
+        string name, value;
+        int index;
+        ss >> name >> index >> value;
+        doubly_lists[name].push_in(index, value);
+    }
+    else if (command == "2LPOP_HEAD"){ // удаление элемента с головы
+        string name;
+        ss >> name;
+        doubly_lists[name].pop_forward();
+    }
+    else if (command == "2LPOP_TAIL"){ // удаление элемента с хвоста
+        string name;
+        ss >> name;
+        doubly_lists[name].pop_backward();
+    }
+    else if (command == "2LPOP"){ // удаление элемента по индексу
+        string name;
+        int index;
+        ss >> name >> index;
+        doubly_lists[name].pop(index);
+    }
+    else if (command == "2LPOP_VALUE"){ // удаление элемента по значению
+        string name, value;
+        ss >> name >> value;
+        doubly_lists[name].pop_value(value);
+    }
+    else if (command == "2LSEARCH"){ // поиск элемента по значению
+        string name, value;
+        ss >> name >> value;
+        doubly_lists[name].search(value);
+    }
+    else if (command == "2LPRINT"){ // вывод на экран
+        string name;
+        ss >> name;
+        doubly_lists[name].display();
     }
 
-    std::cout << "--------------AVL BINARY TREE-------------" << std::endl;
+    // ----------QUEUE-------
 
-    AVLTree<int> tree;
-    tree.create_root(10);
-    tree.insert(tree.root, 5);
-    tree.insert(tree.root, 15);
-    tree.insert(tree.root, 3);
-    tree.insert(tree.root, 7);
-    tree.insert(tree.root, 20);
 
-    std::cout << "Дерево, вывод методом левого обхода(от корня вниз до самого мин.):" << std::endl;
-    tree.printTree(tree.root);
-    std::cout << std::endl;
+    else if (command == "QPUSH"){ // Добавление элемента
+        string name, value;
+        ss >> name >> value;
+        queues[name].push(value);
+    }
+    else if (command == "QPOP"){ // удаление элемента
+        string name;
+        ss >> name;
+        queues[name].pop();
+    }
+    else if (command == "QSEARCH"){ // поиск элемента
+        string name, value;
+        ss >> name >> value;
+        queues[name].search(value);
+    }
+    else if (command == "QPRINT"){ //вывод на экран
+        string name;
+        ss >> name;
+        queues[name].display();
+    }
+    
+    // ----------STACK-------
 
-    tree_node<int>* min_node = tree.find_min_data(tree.root);
-    std::cout << "Минимальное значение: " << min_node->data << std::endl;
+    else if (command == "SPUSH"){ // Добавление элемента
+        string name, value;
+        ss >> name >> value;
+        stacks[name].push(value);
+    }
+    else if (command == "SPOP"){ // удаление элемента
+        string name;
+        ss >> name;
+        stacks[name].pop();
+    }
+    else if (command == "SSEARCH"){ // поиск элемента
+        string name, value;
+        ss >> name >> value;
+        stacks[name].search(value);
+    }
+    else if (command == "SPRINT"){ //вывод на экран
+        string name;
+        ss >> name;
+        stacks[name].display();
+    }
 
-    tree_node<int>* new_root = tree.remove_min_data(tree.root);
-    tree.root = new_root;
+    // ----------HASH-TABLE-------
+    // get_value(string key, string value)
+    // insert(string key, string value)
+    // remove(string key)
 
-    std::cout << "Дерево,после удаления:" << std::endl;
-    tree.printTree(tree.root);
-    std::cout << std::endl;
+    else if (command == "HPUSH"){ // Добавление элемента
+        string name, key, value;
+        ss >> name >> key >> value;
+        hash_tables[name].insert(key, value);
+    }
+    else if (command == "HPOP"){ // Удаление элемента
+        string name, key;
+        ss >> name >> key;
+        hash_tables[name].remove(key);
+    }
+    else if (command == "HGET"){ // Получение значения по ключу
+        string name, key, value;
+        ss >> name >> key >> value;
+        hash_tables[name].get_value(key, value);
+    }
 
-    tree.remove(tree.root, 7);
+    // ----------TREE-------
+    // create_root(int data) // создание корня
+    // insert(tree_node<Data>* _node, int data) // вставка ключа data в дерево
+    // find(tree_node<Data>* _node, int data) // поиск узла с нужным значением
+    // remove(tree_node<Data>* _node, int data) // удаление узла с нужным значением
+    // printTree(tree_node<Data>* node) // вывод дерева на экран
 
-    std::cout << "Дерево, после удаления конкретного значения:" << std::endl;
-    tree.printTree(tree.root);
-    std::cout << std::endl;
+    else if (command == "TROOT"){
+        string name;
+        int root;
+        ss >> name >> root;
+        trees[name].create_root(root);
+    }
+    else if (command == "TINSERT"){
+        string name;
+        int data;
+        ss >> name >> data;
+        trees[name].insert(trees[name].root, data);
+    }
+    else if (command == "TSEARCH"){
+        string name;
+        int data;
+        ss >> name >> data;
+        trees[name].find(trees[name].root, data);
+    }
+    else if (command == "TPOP"){
+        string name;
+        int data;
+        ss >> name >> data;
+        trees[name].root = trees[name].remove(trees[name].root, data);
+    }
+    else if (command == "TPRINT"){
+        string name;
+        ss >> name;
+        trees[name].printTree(trees[name].root);
+    }
+    else {
+        cout << "Неизвестная команда: " << command << endl;
+    }
+}
+
+// Загрузка данных из файла
+void loadFromFile(const string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cout << "Ошибка открытия файла: " << filename << endl;
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        processQuery(line);
+    }
+    file.close();
+}
+
+// Сохранение данных в файл
+template <typename T>
+void saveToFile(const string& filename) {
+    ofstream file(filename);
+    if (!file) {
+        cout << "Ошибка открытия файла: " << filename << endl;
+        return;
+    }
+
+    // Сохранение массивов
+    for (const auto& [name, array] : arrays) {
+        for (int i = 0; i < array.size; ++i) {
+            file << name << " " << array.array[i] << endl;
+        }
+    }
+
+    // Сохранение связных списков
+    for (const auto& [name, linked_list] : linked_lists) {
+        Node<string>* current = linked_list.head;
+        while (current != nullptr) {
+            file << name << " " << current->data << endl;
+            current = current->next;
+        }
+    }
+
+        // Сохранение двусвязных списков
+    for (const auto& [name, doubly_list] : doubly_lists) {
+        Node<string>* currentDoubly = doubly_list.head;
+        while (currentDoubly != nullptr) {
+            file << name << " " << currentDoubly->data << endl;
+            currentDoubly = currentDoubly->next;
+        }
+    }
+
+    // Сохранение очередей
+    for (const auto& [name, queue] : queues) {
+        Node<string>* currentQueue = queue.head;
+        while (currentQueue != nullptr) {
+            file << name << endl; // Просто указываем имя очереди, так как QPOP удаляет элемент
+            currentQueue = currentQueue->next;
+        }
+    }
+
+    // Сохранение стеков
+    for (const auto& [name, stack] : stacks) {
+        Node<string>* currentStack = stack.head;
+        while (currentStack != nullptr) {
+            file << name << " " << currentStack->data << endl;
+            currentStack = currentStack->next;
+        }
+    }
+
+    // Сохранение хэш-таблиц
+    for (const auto& [name, hash_table] : hash_tables) {
+        for (int i = 0; i < 1000; ++i) {
+            Hash_node<string, string>* item = hash_table.table[i];
+            while (item != nullptr) {
+                file << name << " " << item->key << " " << item->value << endl;
+                item = item->next;
+            }
+        }
+    }
+
+        // Сохранение деревьев
+    for (const auto& [name, tree] : trees) {
+        // TODO: реализовать сохранение дерева
+    }
+
+    file.close();
+}
+
+int main(int argc, char* argv[]) {
+    system("chcp 65001");
+    string filename;
+
+    // Обработка аргументов командной строки
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+        if (arg == "--file") {
+            if (i + 1 < argc) {
+                filename = argv[++i];
+            }
+        }
+    }
+
+    // Загружаем данные из файла
+    if (!filename.empty()) {
+        loadFromFile(filename);
+    }
+
+    // Интерактивный ввод команд
+    string query;
+    cout << "Введите команды (или 'exit' для выхода):" << endl;
+    while (true) {
+        cout << "> ";
+        getline(cin, query);
+        if (query == "exit") {
+            break; // Выход из цикла
+        }
+        processQuery(query);
+    }
+
+    // Сохраняем данные обратно в файл
+    if (!filename.empty()) {
+        saveToFile<string>(filename);
+    }
 
     return 0;
 }
